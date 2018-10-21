663676fc9ec6846158c3bc624df67ab614926b63
Preeti Yuankrathok <preetisatit@gmail.com>
2018-10-21 17:21:58 +0700

    add parser untested

--- remover.py
+++ remover.py
@@ -1,6 +1,17 @@
-def main():
-    pass
+import argparse
+
+from openpyxl.reader.excel import load_workbook
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('file_name', type=str, help='Excel file name')
+
+
+def main(args):
+    wb = load_workbook()
 
 
 if __name__ == '__main__':
-    main()
+    args = parser.parse_args()
+    print(type(args))
+    # main(args)
